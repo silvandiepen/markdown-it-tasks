1e2d17797a30ce7a9d3931c0a205777737c157fb
Sil van Diepen <me@sil.mt>
2021-02-06 22:06:29 +0100

    add more unit tests

--- src/index.test.ts
+++ src/index.test.ts
@@ -130,10 +130,10 @@ describe("Run Markdown", () => {
 
     const input = `# My first file
 
-  - [X] This
-  - [x] Is A
-  - [X] Basic list
-  `;
+- [X] This
+- [x] Is A
+- [X] Basic list
+`;
     const result = md.render(input);
     const output = `<h1>My first file</h1>
 <ul class="task-list">
@@ -167,6 +167,88 @@ describe("Run Markdown", () => {
 <li class="task-list__item"><label class="task-list__label" for="task-list__item--4999000">This</label><input class="task-list__input" type="checkbox" checked disabled id="task-list__item--4999000"></li>
 <li class="task-list__item"><label class="task-list__label" for="task-list__item--4999000">Is A</label><input class="task-list__input" type="checkbox" checked disabled id="task-list__item--4999000"></li>
 <li class="task-list__item"><label class="task-list__label" for="task-list__item--4999000">Basic list</label><input class="task-list__input" type="checkbox" checked disabled id="task-list__item--4999000"></li>
+</ul>
+`;
+
+    expect(result).toStrictEqual(output);
+  });
+  it("Should create a task list with enabled labels in the beginning", () => {
+    const md = new MarkdownIt();
+    md.use(Tasks, {
+      enabled: true,
+      label: true,
+      labelAfter: false,
+      labelBefore: true,
+    });
+
+    const input = `# My first file
+
+- [X] This
+- [x] Is A
+- [X] Basic list
+`;
+    const result = md.render(input);
+    const output = `<h1>My first file</h1>
+<ul class="task-list">
+<li class="task-list__item task-list__item--enabled"><label class="task-list__label" for="task-list__item--4999000">This</label><input class="task-list__input" type="checkbox" checked id="task-list__item--4999000"></li>
+<li class="task-list__item task-list__item--enabled"><label class="task-list__label" for="task-list__item--4999000">Is A</label><input class="task-list__input" type="checkbox" checked id="task-list__item--4999000"></li>
+<li class="task-list__item task-list__item--enabled"><label class="task-list__label" for="task-list__item--4999000">Basic list</label><input class="task-list__input" type="checkbox" checked id="task-list__item--4999000"></li>
+</ul>
+`;
+
+    expect(result).toStrictEqual(output);
+  });
+  it("Should create a task list with enabled labels at the end", () => {
+    const md = new MarkdownIt();
+    md.use(Tasks, {
+      enabled: true,
+      label: true,
+      labelAfter: true,
+      labelBefore: false,
+    });
+
+    const input = `# My first file
+
+- [ ] This
+- [ ] Is A
+- [ ] Basic list
+`;
+    const result = md.render(input);
+    const output = `<h1>My first file</h1>
+<ul class="task-list">
+<li class="task-list__item task-list__item--enabled"><input class="task-list__input" type="checkbox" id="task-list__item--4999000"><label class="task-list__label" for="task-list__item--4999000">This</label></li>
+<li class="task-list__item task-list__item--enabled"><input class="task-list__input" type="checkbox" id="task-list__item--4999000"><label class="task-list__label" for="task-list__item--4999000">Is A</label></li>
+<li class="task-list__item task-list__item--enabled"><input class="task-list__input" type="checkbox" id="task-list__item--4999000"><label class="task-list__label" for="task-list__item--4999000">Basic list</label></li>
+</ul>
+`;
+
+    expect(result).toStrictEqual(output);
+  });
+  it("Should create an enabled task list with custom classes", () => {
+    const md = new MarkdownIt();
+    md.use(Tasks, {
+      enabled: true,
+      label: true,
+      containerClass: "list",
+      labelClass: "label",
+      labelBefore: true,
+      labelAfter: false,
+      itemClass: "item",
+      inputClass: "input",
+    });
+
+    const input = `# My first file
+
+- [X] This
+- [x] Is A
+- [X] Basic list
+  `;
+    const result = md.render(input);
+    const output = `<h1>My first file</h1>
+<ul class="list">
+<li class="item item--enabled"><label class="label" for="item--4999000">This</label><input class="input" type="checkbox" checked id="item--4999000"></li>
+<li class="item item--enabled"><label class="label" for="item--4999000">Is A</label><input class="input" type="checkbox" checked id="item--4999000"></li>
+<li class="item item--enabled"><label class="label" for="item--4999000">Basic list</label><input class="input" type="checkbox" checked id="item--4999000"></li>
 </ul>
 `;
 
